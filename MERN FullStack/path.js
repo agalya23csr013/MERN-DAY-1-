07bfb9e43c42e487bc75ba45817073dc284a3d13
AGALYA  S <155147199+agalya23csr013@users.noreply.github.com>
2025-12-12 09:15:40 +0530

    Delete MERN FullStack/path.js

--- MERN FullStack/path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-basedName=path.basename('C:\Users\agaly\OneDrive\Documents\MERN FullStack\nodejs\sample.txt')
-console.log(basedName);
-
-const extensionName=path.extname('C:\Users\agaly\OneDrive\Documents\MERN FullStack\nodejs\sample.txt')
-console.log(extensionName);
-
-const joinedPath=path.join('C:\Users\agaly\OneDrive\Documents\MERN FullStack\nodejs','sample.txt')
-console.log(joinedPath);
